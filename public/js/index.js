3c4fe2d29fb1c63b081f132126bd9eb960fcef6a
Gaurav Soni <gaurav.soni@sprinklr.com>
2017-02-03 11:37:04 +0530

    React basic code layout

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,48 @@
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+import { render} from 'react-dom'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      ReminderText:true
+    }
+    this.timer = this.timer.bind(this)
+  }
+
+  timer(ms) {
+  return new Promise((resolve) => {
+    setTimeout(()=> {
+      resolve(this.setState({ReminderText:!this.state.ReminderText}));
+    }, ms);
+  });
+ }
+
   render() {
+    this.timer(2000);
+    var ReminderText=(
+      <div className="MainAppTile">
+        <div className="ReminderText">
+          <span>It's Time To Dance</span>
+        </div>
+      </div>
+    );
+
+    var Timer=(
+      <div className="MainAppTile">
+        <div className="ReminderText" >
+          <span>Heldlo</span>
+        </div>
+      </div>
+    );
+
     return (
       <div>
-        WIP
+         { this.state.ReminderText ? ReminderText: Timer }
       </div>
-    )
-  }
+   )
+  };
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+render(<App />, document.querySelector('#root'))
